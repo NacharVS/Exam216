d5fc831c0a088afe8796f9e785ef10bc5875983b
r3dinc <r3dinc@yandex.ru>
2020-12-19 15:04:46 +0300

    master start

---
+++ Exam216/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leave hope behind who enters here...");
+        }
+    }
+}
