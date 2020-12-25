f075b002c4c49e94d0da1b4f603544fa7d58f60b
Azaliya <amalkaazaliya@gmail.com>
2020-12-25 15:34:04 +0300

    Good!))

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -6,7 +6,24 @@ namespace Exam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] array = new int[10, 10];
+            Random rnb = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int f = 0; f < array.GetLength(1); f++)
+                {
+                    array[i, f] = rnb.Next(10, 30);
+                    Console.Write($" {array[i, f]}");
+                }
+                Console.WriteLine();
+            }
+            int Sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                
+                Sum = Sum + array[i, i];
+            }
+            Console.WriteLine($"The sum of the main diagonal: {Sum}");
         }
     }
 }
