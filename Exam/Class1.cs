d7b47e382fec3f0cc07f17988c622616740021e7
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-12-25 16:41:37 +0300

    2121

---
+++ Exam/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam
+{
+    class Class1
+    {
+            public static void Add(); 
+        {
+              Random rnd = new Random();
+              int[] array = new int[10];
+
+              for(int i =0 ;i<10; i++)
+              {
+                array[i]= rnd.Next(0,25)  ;
+                Console.WriteLine(Array[i]+"");
+              }
+        }
+                static void Main(string[] args)
+               {
+                add();
+               }
+
+    }
+}
