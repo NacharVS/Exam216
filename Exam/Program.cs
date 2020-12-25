10ac3777e743b6b8a277bd3dd74ab698b1f22953
Zulfia <zulfianabiullina4998@.com>
2020-12-25 14:43:01 +0300

    Factorial chisla N

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -6,7 +6,21 @@ namespace Exam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Leave hope behind all who enters here...");
+            Console.WriteLine(Factorial(5));
+            
+        }
+        static int Factorial (int x)
+        {
+            if (x==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+                
+            }
+            
         }
     }
 }
