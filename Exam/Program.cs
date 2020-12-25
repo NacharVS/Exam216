afbf7d4fb80484c468ddda11d96120a5a0dd4906
islam7898 <is.fs.ilsh@gmail.com>
2020-12-25 14:56:40 +0300

    bank

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Exam
 {
-    class Program
+    class BancAccount
     {
-        static void Main(string[] args)
+        class BankAccount
         {
-            Console.WriteLine("Leave hope behind all who enters here...");
+            public string name;
+            public string surname;
+            public int accountNumber;
         }
+        class Program
+        {
+            public static object Enviroment { get; private set; }
+
+            static void Main(string[] args)
+            {
+                BankAccount user = new BankAccount();
+                user.name = Console.ReadLine();
+                user.surname = Console.ReadLine();
+                user.accountNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите 1, если хотите пополнитиь счет" + Enviroment.NewLine +
+                "Введите 2,если хотите снять деньги" + Environment.NewLine +
+                "Введите 3,если хотите перевести деньги на другой счет");
+                
+                int n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                }
     }
 }
