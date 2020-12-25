87ce248033705dc7b9e3e9726b96ecae5e9dc16e
islam7898 <is.fs.ilsh@gmail.com>
2020-12-25 16:21:59 +0300

    1

---
+++ Exam/Characteristics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam
+{
+    abstract class Characteristics
+    {
+        public int _health { get; set; }
+        public int _agility { get; set; }
+        public int _strenght { get; set; }
+        public int _luck { get; set; }
+    }
+}
---
+++ Exam/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam
+{
+    class Person : Characteristics
+    {
+        public string _name;
+        Random rnd = new Random();
+        public Person(int health, int agility, int strenght, int luck, string name)
+        {
+            _name = name;
+            _health = health;
+            _agility = agility;
+            _strenght = strenght;
+            _luck = luck;
+        }
+        public void Attack(Person person)
+        {
+            int dmg = rnd.Next(50, 100) + _strenght;
+            person._health -= dmg;
+            Console.WriteLine($"{_name} нанёс {dmg} урона {person.name}");
+        }
+        public static void Start()
+        {
+            Person person1 = new Person(health: 1000, agility: 10, strenght: 10, luck: 5, name:"ChuvakOdin");
+            Person person2 = new Person(health: 1000, agility: 10, strenght: 10, luck: 10, name: "ChuvakDva");
+
+            while (true)
+            {
+                if (Person.health > 0 )
+                {
+                    person2.Attack(person1);
+                }
+                else
+                {
+                    Console.WriteLine($"{person1._name} мёртв");
+
+                }
+            }
+        }
+    }
+}
+
